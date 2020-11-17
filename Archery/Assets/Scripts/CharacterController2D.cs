b56697812c1f2fce8fff07fc7b6da04d6a769fc6
marc2036 <marcnielsen.business@gmail.com>
2020-11-17 18:08:50 +0100

    Initial Commit

---
+++ Archery/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterController2D : MonoBehaviour
+{
+    float hort;
+    float vert;
+
+    float speed;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hort = Input.GetAxisRaw("Horizontal");
+        //vert = Input.GetAxisRaw("Vertical");
+
+        anim.SetBool("Drawn", Input.GetButton("Fire1"));
+    }
+}
